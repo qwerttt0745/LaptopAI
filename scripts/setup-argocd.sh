f71657dae7854f9532ce9e73c90ac9eae2fec901
qwerttt0745 <olexiy2005@gmail.com>
2026-03-22 12:39:36 +0000

    fix

--- scripts/setup-argocd.sh
+++ scripts/setup-argocd.sh
@@ -5,6 +5,13 @@ ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret \
   -n argocd \
   -o jsonpath="{.data.password}" | base64 -d)
 
+ARGOCD_NEW_PASSWORD=$(aws ssm get-parameter \
+  --name "/laptopai/dev/argocd-password" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text \
+  --region eu-central-1)
+
 argocd login localhost:9090 \
   --username admin \
   --password "$ARGOCD_PASSWORD" \
@@ -12,7 +19,7 @@ argocd login localhost:9090 \
 
 argocd account update-password \
   --current-password "$ARGOCD_PASSWORD" \
-  --new-password 'LaptopAI2024'
+  --new-password "$ARGOCD_NEW_PASSWORD"
 
 argocd repo add https://github.com/qwerttt0745/LaptopAI \
   --username qwerttt0745 \
